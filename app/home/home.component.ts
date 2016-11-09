f38c153eda8fb404bda817c3d7a6d95b5dd0b57a
Artjom Andrunevic <artjomandrunevic@Artjoms-MacBook-Pro.local>
2016-11-09 09:39:23 +0100

    änderung frodo content

--- app/home/home.component.ts
+++ app/home/home.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
 selector: 'home-page',
-template: `ich bin frodo`
+template: `ich bin frodo und so ...
+<div> Hallo Frodo</div>`
 })
 
 export class HomeComponent {} 
